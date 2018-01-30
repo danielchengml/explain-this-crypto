757eab58fe56d7470d638eb491139b985a4cb2de
Daniel Cheng <danielchengml@gmail.com>
2018-01-30 14:42:17 -0800

    Create Template MVC and connected database with coin and description tables migrated

---
+++ ExplainThisCrypto/Models/Coin.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExplainThisCrypto.Models
+{
+    [Table("Coins")]
+    public class Coin
+    {
+        [Key]
+        public int CoinId { get; set; }
+        public string Name { get; set; }
+        public string Symbol { get; set; }
+        public string Logo_url { get; set; }
+        public virtual ApplicationUser User { get; set; }
+        public virtual ICollection<Description> Descriptions { get; set; }
+
+        public Coin(string name, string symbol, string logo_url)
+        {
+            Name = name;
+            Symbol = symbol;
+            Logo_url = logo_url;
+        }
+
+        public Coin()
+        {
+        }
+    }
+}
---
+++ ExplainThisCrypto/Models/Description.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExplainThisCrypto.Models
+{
+    [Table("Descriptions")]
+    public class Description
+    {
+        [Key]
+        public int DescriptionId { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+        public virtual ApplicationUser User { get; set; }
+        public virtual Coin Coins { get; set; }
+
+        public Description(string content, string author)
+        {
+            Content = content;
+            Author = author;
+        }
+
+        public Description()
+        {
+        }
+    }
+}
