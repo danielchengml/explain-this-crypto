0de14e8dfab4bc7fa9335107a7f10542bd9278a9
Daniel Cheng <danielchengml@gmail.com>
2018-01-30 15:20:20 -0800

    add Coinscontroller, CRUD and Views

---
+++ ExplainThisCrypto/Controllers/CoinsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ExplainThisCrypto.Data;
+using ExplainThisCrypto.Models;
+
+namespace ExplainThisCrypto.Controllers
+{
+    public class CoinsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CoinsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Coins
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Coins.ToListAsync());
+        }
+
+        // GET: Coins/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var coin = await _context.Coins
+                .SingleOrDefaultAsync(m => m.CoinId == id);
+            if (coin == null)
+            {
+                return NotFound();
+            }
+
+            return View(coin);
+        }
+
+        // GET: Coins/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Coins/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("CoinId,Name,Symbol,Logo_url")] Coin coin)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(coin);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(coin);
+        }
+
+        // GET: Coins/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var coin = await _context.Coins.SingleOrDefaultAsync(m => m.CoinId == id);
+            if (coin == null)
+            {
+                return NotFound();
+            }
+            return View(coin);
+        }
+
+        // POST: Coins/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("CoinId,Name,Symbol,Logo_url")] Coin coin)
+        {
+            if (id != coin.CoinId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(coin);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CoinExists(coin.CoinId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(coin);
+        }
+
+        // GET: Coins/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var coin = await _context.Coins
+                .SingleOrDefaultAsync(m => m.CoinId == id);
+            if (coin == null)
+            {
+                return NotFound();
+            }
+
+            return View(coin);
+        }
+
+        // POST: Coins/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var coin = await _context.Coins.SingleOrDefaultAsync(m => m.CoinId == id);
+            _context.Coins.Remove(coin);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CoinExists(int id)
+        {
+            return _context.Coins.Any(e => e.CoinId == id);
+        }
+    }
+}
