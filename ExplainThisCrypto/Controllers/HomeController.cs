8b7db1bfc24253f33c87bc9c49edf1c1c2501ae6
Daniel Cheng <danielchengml@gmail.com>
2018-01-31 14:50:26 -0800

    add prelim wells for the index page

--- ExplainThisCrypto/Controllers/HomeController.cs
+++ ExplainThisCrypto/Controllers/HomeController.cs
@@ -5,26 +5,38 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ExplainThisCrypto.Models;
+using ExplainThisCrypto.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace ExplainThisCrypto.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var coins = _context.Coins
+                               .Include(x => x.Descriptions)
+                               .ToList();
+            return View(coins);
         }
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "";
 
             return View();
         }
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
+            ViewData["Message"] = "";
 
             return View();
         }
