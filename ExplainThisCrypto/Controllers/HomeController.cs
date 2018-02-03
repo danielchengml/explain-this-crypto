bd6b7bf250187bb5c712c2ddfd2224542b1de3d8
Daniel Cheng <danielchengml@gmail.com>
2018-02-03 13:18:24 -0800

    Featured Coins Functions on HomePage Carousell Unformatted

--- ExplainThisCrypto/Controllers/HomeController.cs
+++ ExplainThisCrypto/Controllers/HomeController.cs
@@ -43,7 +43,7 @@ namespace ExplainThisCrypto.Controllers
 
         public IActionResult GetFeaturedCoins()
         {
-            var FeaturedCoinsList = _context.Coins.OrderBy(r => Guid.NewGuid()).Take(4);
+            var FeaturedCoinsList = _context.Coins.OrderBy(r => Guid.NewGuid()).Take(4).ToList();
             return Json(FeaturedCoinsList);
         }
 
