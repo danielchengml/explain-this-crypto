5ba0ae3458b898c2d0e3dd1e605e812926a7165c
Daniel Cheng <danielchengml@gmail.com>
2018-02-09 19:05:52 -0800

    add Source Column to DescriptionsTable

---
+++ ExplainThisCrypto/Migrations/20180210030306_SourceColumnDescriptions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace ExplainThisCrypto.Migrations
+{
+    public partial class SourceColumnDescriptions : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Source",
+                table: "Descriptions",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Source",
+                table: "Descriptions");
+        }
+    }
+}
--- ExplainThisCrypto/Models/Description.cs
+++ ExplainThisCrypto/Models/Description.cs
@@ -14,14 +14,16 @@ namespace ExplainThisCrypto.Models
         public int DescriptionId { get; set; }
         public string Content { get; set; }
         public string Author { get; set; }
+        public string Source { get; set; }
         public int CoinId { get; set; }
         public virtual ApplicationUser User { get; set; }
         public virtual Coin Coins { get; set; }
 
-        public Description(string content, string author, int coinId)
+        public Description(string content, string author, string source, int coinId)
         {
             Content = content;
             Author = author;
+            Source = source;
             CoinId = coinId;
         }
 
