f6fc30975dfa197599b3d9aa5fc3276ddff3b3b9
Daniel Cheng <danielchengml@gmail.com>
2018-02-17 11:28:40 -0800

    seedAdmin

--- ExplainThisCrypto/Controllers/AccountController.cs
+++ ExplainThisCrypto/Controllers/AccountController.cs
@@ -25,15 +25,19 @@ namespace ExplainThisCrypto.Controllers
         private readonly IEmailSender _emailSender;
         private readonly ILogger _logger;
 
+        private RoleManager<IdentityRole> RoleManager;
+
         public AccountController(
             UserManager<ApplicationUser> userManager,
             SignInManager<ApplicationUser> signInManager,
             IEmailSender emailSender,
-            ILogger<AccountController> logger)
+            ILogger<AccountController> logger,
+            RoleManager<IdentityRole> roleManager)
         {
             _userManager = userManager;
             _signInManager = signInManager;
             _emailSender = emailSender;
+            RoleManager = roleManager;
             _logger = logger;
         }
 
@@ -224,11 +228,21 @@ namespace ExplainThisCrypto.Controllers
                 var result = await _userManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
-                    _logger.LogInformation("User created a new account with password.");
+                    if (!await RoleManager.RoleExistsAsync("Admin"))
+                    {
+                        var users = new IdentityRole("Admin");
+                        var res = await RoleManager.CreateAsync(users);
+                        if (res.Succeeded)
+                        {
+                            await _userManager.AddToRoleAsync(user, "Admin");
+                        }
+                    }
 
-                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                    var callbackUrl = Url.EmailConfirmationLink(user.Id, code, Request.Scheme);
-                    await _emailSender.SendEmailConfirmationAsync(model.Email, callbackUrl);
+                   // _logger.LogInformation("User created a new account with password.");
+
+                   // var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                    //var callbackUrl = Url.EmailConfirmationLink(user.Id, code, Request.Scheme);
+                   // await _emailSender.SendEmailConfirmationAsync(model.Email, callbackUrl);
 
                     await _signInManager.SignInAsync(user, isPersistent: false);
                     _logger.LogInformation("User created a new account with password.");
